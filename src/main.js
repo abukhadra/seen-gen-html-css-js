7cdfd9e1f0fb866e2008e524bb20d1e0db8c51e0
Mustafa Abukhadra <abukhadra@gmail.com>
2024-09-29 13:02:48 +0300

    update

--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ import {JSGen} from '../lib/sgen-js.js'
 
 class HtmlCssJSGen {
     run(ast, symtab, main_args, opts) {
-        const html_gen = { en: new HtmlWriter(JSGen()), ar: new ArHtmlWriter(JSGen()) }
+        const html_gen = { 
+            en: new HtmlWriter(new JSGen()), 
+            ar: new ArHtmlWriter(new JSGen()) 
+        }
 
         const js_gen = new JSGen()
         js_gen.init(
